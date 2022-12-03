c39929e274b73f90287f52793f885b6515a308b3
Evgeny Berg <berg.evg.1@gmail.com>
2022-12-04 00:43:39 +0400

    refactor: refactored classes and swaped clouds

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { RouterProvider } from 'react-router-dom';
+import { darkMode } from 'tailwindcss/defaultConfig'
 
 import router from './router';
 
 function App() {
+    console.log(darkMode);
     return (
         <div className={`h-screen w-screen overflow-hidden`}>
             <RouterProvider router={router} />
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -12,62 +12,62 @@ function Navbar() {
 
     useEffect(() => {
         if (activeLinkBackgroundRef.current) {
-            activeLinkBackgroundRef.current.style.left = `${
-                activeLinkRef.current?.offsetLeft || 0
-            }px`;
-            activeLinkBackgroundRef.current.style.width = `${
-                activeLinkRef.current?.offsetWidth || 0
-            }px`;
-            if (!activeLinkBackgroundRef.current.style.height) {
-                activeLinkBackgroundRef.current.style.height = `${
-                    activeLinkRef.current?.offsetHeight || 0
+            activeLinkBackgroundRef.current.style.left = `${activeLinkRef.current?.offsetLeft || 0
                 }px`;
+            activeLinkBackgroundRef.current.style.width = `${activeLinkRef.current?.offsetWidth || 0
+                }px`;
+            if (!activeLinkBackgroundRef.current.style.height) {
+                activeLinkBackgroundRef.current.style.height = `${activeLinkRef.current?.offsetHeight || 0
+                    }px`;
             }
         }
     }, [location.pathname]);
 
     return (
         <div
-            className={`w-full bg-blue-700 h-12 grid grid-cols-3 items-center min-w-full px-4 text-white`}
+            className={'w-full bg-blue-700 h-12 grid grid-cols-3 items-center min-w-full px-4 text-white'}
         >
             <NavLink
                 to="/"
-                className={`group relative justify-self-start h-full grid place-items-center aspect-square w-fit`}
+                className={'group relative justify-self-start h-full grid place-items-center aspect-square w-fit'}
             >
                 {({ isActive, isPending }) => (
                     <>
                         <Sun
-                            className={`absolute h-2/3 transition-[fill] fill-yellow-300 ${
-                                isActive && 'animate-spin-5s'
-                            } ${isPending && 'animate-spin-3s'}`}
+                            className={'absolute h-2/3 transition-[fill] fill-yellow-300'
+                                .concat(isActive ? ' animate-spin-5s' : '')
+                                .concat(isPending ? ' animate-spin-3s' : '')
+                            }
                         />
                         <CloudLeft
-                            className={`absolute z-[1] h-2/3 transition-transform fill-slate-400 w-fit translate-x-2 translate-y-1 delay-100 duration-[275px] ${
-                                isActive && 'translate-x-[1.625rem]'
-                            } group-hover:translate-x-6`}
+                            className={
+                                'absolute z-[2] h-2/3 transition-transform fill-slate-400 w-fit -translate-x-2 translate-y-1 delay-100 duration-[275px]'
+                                    .concat(' group-hover:-translate-x-6')
+                                    .concat(isActive ? ' -translate-x-[1.625rem]' : '')
+                            }
                         />
                         <CloudRight
-                            className={`absolute z-[2] h-2/3 transition-transform fill-slate-300 w-fit -translate-x-2 translate-y-1 scale-95 delay-200 duration-[375ms] shadow-smp ${
-                                isActive && '-translate-x-[1.625rem]'
-                            } group-hover:-translate-x-6`}
+                            className={
+                                'absolute z-[1] h-2/3 transition-transform fill-slate-300 w-fit translate-x-2 translate-y-1 scale-95 delay-200 duration-[375ms]'
+                                    .concat(' group-hover:translate-x-6')
+                                    .concat(isActive ? ' translate-x-[1.625rem]' : '')
+                            }
                         />
                     </>
                 )}
             </NavLink>
-            <div className={`relative flex gap-2 p-2 justify-self-center`}>
+            <div className={'relative flex gap-2 p-2 justify-self-center'}>
                 <div
                     ref={activeLinkBackgroundRef}
-                    className={`absolute bg-white h-8 rounded-lg aspect-square transition-[left,_width,_height] duration-300 pointer-events-none`}
+                    className={'absolute bg-white h-8 rounded-lg aspect-square transition-[left,_width,_height] duration-300 pointer-events-none'}
                 ></div>
                 <NavLink
                     to="/current"
                     ref={location.pathname === '/current' ? activeLinkRef : null}
                     className={({ isActive, isPending }) =>
-                        `relative z-[1] px-2 py-1 rounded-lg transition-colors delay-200 underline decoration-transparent ${
-                            !isActive && 'hover:decoration-white'
-                        } ${isActive && 'text-black'} ${
-                            isPending && 'pointer-events-none bg-gray-500'
-                        }`
+                        'relative z-[1] px-2 py-1 rounded-lg transition-colors delay-200 underline decoration-transparent'
+                            .concat(' ', isActive ? 'text-black' : 'hover:decoration-white')
+                            .concat(isPending ? ' pointer-events-none bg-gray-500' : '')
                     }
                 >
                     Current
@@ -76,17 +76,15 @@ function Navbar() {
                     to="/test"
                     ref={location.pathname === '/test' ? activeLinkRef : null}
                     className={({ isActive, isPending }) =>
-                        `relative z-[1] px-2 py-1 rounded-lg transition-colors delay-200 underline decoration-transparent ${
-                            !isActive && 'hover:decoration-white'
-                        } ${isActive && 'text-black'} ${
-                            isPending && 'pointer-events-none bg-gray-500'
-                        }`
+                        'relative z-[1] px-2 py-1 rounded-lg transition-colors delay-200 underline decoration-transparent'
+                            .concat(' ', isActive ? 'text-black' : 'hover:decoration-white')
+                            .concat(isPending ? ' pointer-events-none bg-gray-500' : '')
                     }
                 >
                     Test
                 </NavLink>
             </div>
-        </div>
+        </div >
     );
 }
 
